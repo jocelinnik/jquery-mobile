94764b0efd509189bfcd0012ce13acdaf7a11c43
Linnik Maciel de Souza <linnik.souza@citsamazonas.local>
2020-01-13 20:07:46 -0400

    Mais ajustes

--- jqm-starter/server.js
+++ jqm-starter/server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static("./public"));
 
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.php");
+});
+
 app.listen(3000, function(){
     console.log("SERVIDOR RODANDO VIOLENTAMENTE NA PORTA 3000");
 });
